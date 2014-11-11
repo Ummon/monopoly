49d7ef13fbced8475d3c78634c6cdd3fa3ec31a2
lin <lin@eifrpoe00115.local>
2014-11-11 12:11:35 +0100

    Initial Commit

---
+++ Monopoly/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Monopoly
+//
+//  Created by lin on 11/11/14.
+//  Copyright (c) 2014 HES-SO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
